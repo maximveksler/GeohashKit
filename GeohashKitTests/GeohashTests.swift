1da40009ef031851635d9e571fba1a41769512bb
Maxim Veksler <maxim@vekslers.org>
2015-05-05 17:59:23 +0300

    Unit testing now works.

--- GeohashKitTests/GeohashTests.swift
+++ GeohashKitTests/GeohashTests.swift
@@ -2,15 +2,19 @@
 //  GeohashTests.swift
 //  GeohashKit
 //
+//  Tested are based on https://github.com/vyshane/geohash-tool/tree/master/Geohash%20Tool
+//
 //  Created by Maxim Veksler on 5/5/15.
-//  Copyright (c) 2015 Maxim Veksler. All rights reserved.
+//  (c) 2015 Maxim Veksler.
 //
 
 import XCTest
+import GeohashKit
 
 class GeohashTests: XCTestCase {
     func testEncode() {
-        Geohash 
+        XCTAssertEqual(Geohash.encodeGeoHash(-31.953, longitude: 115.857, precision: 8), "qd66hrhk")
+        XCTAssertEqual(Geohash.encodeGeoHash(38.89710201881826, longitude: -77.03669792041183, precision: 12), "dqcjqcp84c6e")
     }
 }
 
