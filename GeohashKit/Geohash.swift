f0bc81d396f371bdc250c7307d7a150c570d7b70
Maxim Veksler <maxim@vekslers.org>
2015-05-07 21:15:10 +0300

    Account for unspecified precision by supplying sane defaults.

--- GeohashKit/Geohash.swift
+++ GeohashKit/Geohash.swift
@@ -21,7 +21,7 @@ prefix func !(a: Parity) -> Parity {
     return a == .Even ? .Odd : .Even
 }
 
-let BITS = [16, 8, 4, 2, 1]
+let BITS = [1<<4, 1<<3, 1<<2, 1<<1, 1]
 let BASE32 = Array("0123456789bcdefghjkmnpqrstuvwxyz")
 
 let NEIGHBORS : [CompassPoint : [Parity : String]] = [
@@ -47,7 +47,7 @@ let BORDERS : [CompassPoint : [Parity : String]] = [
 ]
 
 public class Geohash {
-    public static func encodeGeoHash(latitude: Double, longitude: Double, precision: Int = 12) -> String {
+    public static func encodeGeoHash(#latitude: Double, longitude: Double, _ precision: Int? = Optional.None) -> String {
         var lat = (-90.0, 90.0)
         var lon = (-180.0, 180.0)
         
@@ -55,16 +55,17 @@ public class Geohash {
         var geohash = String()
         
         // Loop helpers
-        var __mode_bit = Parity.Even;
-        var ch = 0
+        var parity_mode = Parity.Even;
+        var base32char = 0
         var bit = 0
         
-        while(count(geohash) < precision) {
-            switch (__mode_bit) {
+        //while(count(geohash) < precision) {
+        do {
+            switch (parity_mode) {
             case .Even:
                 let mid = (lon.0 + lon.1) / 2
                 if(longitude > mid) {
-                    ch |= BITS[bit]
+                    base32char |= BITS[bit]
                     lon.0 = mid;
                 } else {
                     lon.1 = mid;
@@ -72,7 +73,7 @@ public class Geohash {
             case .Odd:
                 let mid = (lat.0 + lat.1) / 2
                 if(latitude > mid) {
-                    ch |= BITS[bit]
+                    base32char |= BITS[bit]
                     lat.0 = mid;
                 } else {
                     lat.1 = mid;
@@ -80,16 +81,18 @@ public class Geohash {
             }
             
             // Flip between Even and Odd
-            __mode_bit = !__mode_bit
+            parity_mode = !parity_mode
             
             if(bit < 4) {
                 bit++
             } else {
-                geohash += String(BASE32[ch])
+                geohash += String(BASE32[base32char])
                 bit = 0
-                ch = 0
+                base32char = 0
             }
-        }
+        } while precision != nil ?
+            count(geohash) < precision : // Either precision is specified
+            bit != 0 || (lat.1 - lat.0 > 0.05) || (lon.1 - lon.0 > 0.05) // or we use sane defaults
         
         return geohash
     }
--- GeohashKitTests/GeohashTests.swift
+++ GeohashKitTests/GeohashTests.swift
@@ -13,9 +13,21 @@ import GeohashKit
 
 class GeohashTests: XCTestCase {
     func testEncode() {
-        XCTAssertEqual(Geohash.encodeGeoHash(-31.953, longitude: 115.857, precision: 8), "qd66hrhk")
-        XCTAssertEqual(Geohash.encodeGeoHash(38.89710201881826, longitude: -77.03669792041183, precision: 12), "dqcjqcp84c6e")
+        // Based on http://geohash.org/site/tips.html, values different a tidbit, because I think geohash.org calculated using 32bit double.
+        XCTAssertEqual(Geohash.encodeGeoHash(latitude: -25.383, longitude: -49.266, 8), "6gkzwgjt")
+        XCTAssertEqual(Geohash.encodeGeoHash(latitude: -25.382708, longitude: -49.265506, 12), "6gkzwgjzn820")
+        XCTAssertEqual(Geohash.encodeGeoHash(latitude: -25.427, longitude: -49.315, 8), "6gkzmg1u")
+
+        // Locations barrowed from Geohash Tool
+        XCTAssertEqual(Geohash.encodeGeoHash(latitude: -31.953, longitude: 115.857, 8), "qd66hrhk")
+        XCTAssertEqual(Geohash.encodeGeoHash(latitude: 38.89710201881826, longitude: -77.03669792041183, 12), "dqcjqcp84c6e")
     }
+    
+    func testEncodeDefaultPrecision() {
+        // Narrow example.
+        XCTAssertEqual(Geohash.encodeGeoHash(latitude: 42.6, longitude: -5.6), "ezs42")
+    }
+
 }
 
 
